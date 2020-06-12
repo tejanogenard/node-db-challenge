c7c29c0bd27061e648f6262c9b475c04dd637089
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-12 08:56:19 -0700

    modified knex and server file

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,19 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/Projects.db3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done); 
+      },
+    },
   },
 
   staging: {
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const helmet = require('helmet')
+const server = express()
 
 const projectRouter = require('./prodjects/projectsRouter')
 const tasksRouter = require('./prodjects/tasks/tasksRouter')
 const resourcesRouter = require('./prodjects /resources/resourcesRouter')
 
+
+server.use(helmet())
+server.use(express.json())
+
 server.use('/api/projects', projectRouter)
 server.use('/api/tasks', tasksRouter)
 server.use('/api/resources', resourcesRouter)
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+module.exports = server
